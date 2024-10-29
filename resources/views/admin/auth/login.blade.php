0d7cbcc39d7a4dc314a68f781b5dc57a67579fbc
coreoceantesting <coreoceantesting@gmail.com>
2024-10-29 18:00:10 +0530

    change title

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>{{ config('app.name') }} | Sign In</title>
+    <title>WHM | Sign In</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
